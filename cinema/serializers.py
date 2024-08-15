add9e7824f111faec7614fcccc9684a37ecbb20c
Anton Bilous <anton.bilosus@gmail.com>
2024-08-15 19:56:41 +0300

    Create MovieSerializer

---
+++ cinema/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from .models import Movie
+
+
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=255, required=True)
+    description = serializers.CharField(required=True)
+    duration = serializers.IntegerField(required=True)
+
+    def create(self, validated_data: dict) -> Movie:
+        return Movie.objects.create(**validated_data)
+
+    def update(self, instance: Movie, validated_data: dict) -> Movie:
+        for field in ("title", "description", "duration"):
+            if field in validated_data:
+                setattr(instance, field, validated_data[field])
+
+        instance.save()
+        return instance
