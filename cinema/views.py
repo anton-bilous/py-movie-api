0d6e479a9122b02ca51042404b53411b88f0743d
Anton Bilous <anton.bilosus@gmail.com>
2024-08-16 18:45:45 +0300

    Create movies api_view

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import Movie
+from .serializers import MovieSerializer
+
+
+@api_view(["GET"])
+def movies(request: HttpRequest) -> Response:
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
